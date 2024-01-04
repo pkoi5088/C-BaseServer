87f6028798f9be8f4d004737084716aa6f10435e
Seoki <pkoi5088@gmail.com>
2024-01-04 20:07:17 +0900

    Thread Manager

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -1,7 +1,26 @@
 #include "pch.h"
 #include <iostream>
 #include "CorePch.h"
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <Windows.h>
+#include <future>
+#include "CoreMacro.h"
+#include "ThreadManager.h"
+
+CoreGlobal Core;
+
+void ThreadMain() {
+	while (true) {
+		cout << "Hello! I am Thread.. " << LThreadId << endl;
+		this_thread::sleep_for(1s);
+	}
+}
 
 int main() {
-	HelloWorld();
+	for (int32 i = 0; i < 5; i++) {
+		GThreadManager->Launch(ThreadMain);
+	}
+	GThreadManager->Join();
 }
